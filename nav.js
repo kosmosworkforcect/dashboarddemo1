9f07d23320b105ffaac0761d56719925ab287aac
kosmosworkforcect <chad.till@kosmosworkforce.com>
2026-05-08 09:03:53 -0500

    Add files via upload

--- nav.js
+++ nav.js
@@ -27,7 +27,7 @@ class Navmenu extends HTMLElement {
 				<path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path>
 				<path d="M16 3.128a4 4 0 0 1 0 7.744"></path><path d="M22 21v-2a4 4 0 0 0-3-3.87"></path>
 				<circle cx="9" cy="7" r="4"></circle>
-			</svg>Employees
+			</svg>My Team
 		</div>
 <!--
 		<div data-link="assessments.html" onclick="window.location.href='assessments.html'">
